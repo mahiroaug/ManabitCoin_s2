2d12b00f0d94650fffcee988208faf7b8be9f28b
mahiroaug <nrs020013@gmail.com>
2023-06-01 08:32:47 +0000

    create web3 functions

--- operation/aws-web3-manabit.js
+++ operation/aws-web3-manabit.js
@@ -4,21 +4,28 @@ require('dotenv').config({ path: 'ca.env'});
 // suppress message while AWS-SDK v2
 require('aws-sdk/lib/maintenance_mode_message').suppress = true;
 
+const network = process.env.NETWORK;
+
+const Web3 = require('web3');
+const AWSHttpProvider = require('./aws-http-provider.js');
+const endpoint = process.env.AMB_HTTP_ENDPOINT;
+const web3 = new Web3(new AWSHttpProvider(endpoint));
+const owner = web3.eth.accounts.privateKeyToAccount(process.env.OWNER_PRIVATE_KEY);
+
+const signer = owner;
+web3.eth.accounts.wallet.add(signer);
 
 const COIN_CA = process.env.MNBC_COIN_CA;
 const GACHA_CA = process.env.MNBC_GACHA_CA;
 const COIN_ABI = require('../artifacts/contracts/ManabitCoin.sol/ManabitCoin.json').abi;
 const GACHA_ABI = require('../artifacts/contracts/ManabitGacha.sol/ManabitGacha.json').abi;
 
-const Web3 = require('web3');
-const AWSHttpProvider = require('./aws-http-provider.js');
-const endpoint = process.env.AMB_HTTP_ENDPOINT;
-const web3 = new Web3(new AWSHttpProvider(endpoint));
+const Coin = new web3.eth.Contract(COIN_ABI, COIN_CA, { from: signer.address });
+const Gacha = new web3.eth.Contract(GACHA_ABI, GACHA_CA, { from: signer.address });
 
-const Coin = new web3.eth.Contract(COIN_ABI, COIN_CA);
-const Gacha = new web3.eth.Contract(GACHA_ABI, GACHA_CA);
-const owner = web3.eth.accounts.privateKeyToAccount(process.env.OWNER_PRIVATE_KEY);
-let signer=owner;
+
+
+////// call functions /////////
 
 async function getOwnerBalance() {
     console.log(`Owner: ${owner.address}`);
@@ -55,49 +62,70 @@ async function getAllowance(spender){
     }
 }
 
+////// send functions /////////
+
+const sendTx = async (_to ,_tx ,_signer,_gasLimit) => {
+
+    // check toAddress
+    toAddress = web3.utils.toChecksumAddress(_to);
+    console.log(' toAddress:',toAddress);
+
+    // gasLimit
+    /* ****** existing bug : ERC20: approce from the zero address
+    const estimateGas = await web3.eth.estimateGas(
+      {
+        to: _to,
+        data: _tx.encodeABI()
+      });
+    const setGasLimit = estimateGas+10000;
+    console.log('gas(estimated):', estimateGas, 'gasLimit:', setgGasLimit);
+    */
+    const setGasLimit = _gasLimit;
+    console.log(' setGasLimit:', setGasLimit);
+
+    // gasPrice
+    const gasPrice = await web3.eth.getGasPrice();
+    const gasPriceInGwei = await web3.utils.fromWei(gasPrice, 'gwei');
+    console.log(' gasPrice:', gasPrice,'(', gasPriceInGwei,'Gwei)');
+
+    // estimate max Transaction Fee
+    const estimateMaxTxFee = setGasLimit * gasPrice;
+    const estimateMaxTxFeeETH = await web3.utils.fromWei(estimateMaxTxFee.toString(), 'ether');
+    console.log(' estimate MAX Tx Fee:', estimateMaxTxFee, '(', estimateMaxTxFeeETH, 'ETH)');
 
 
-async function createSignedTx(receiver,payload,signer,gasLimit){
-    try{
+    // Sign Tx
+    const createTransaction = await web3.eth.accounts.signTransaction(
+      {
+        to: toAddress,
+        data: _tx.encodeABI(),
+        gas: await web3.utils.toHex(_gasLimit)
+      },
+      _signer.privateKey
+    );
+  
+    // Send Tx and Wait for Receipt
+    const createReceipt = await web3.eth
+        .sendSignedTransaction(createTransaction.rawTransaction)
+        .once("transactionHash", (txhash) => {
+            console.log(` Send transaction ...`);
+            console.log(` https://${network}.etherscan.io/tx/${txhash}`);
 
+        })
+    console.log(` Tx successful with hash: ${createReceipt.transactionHash} in block ${createReceipt.blockNumber}`);
+    return(createReceipt);
+};
 
-        //console.log('estimateGas:', gas);
-        
-        const gasPriceInGwei = await web3.utils.fromWei(gasPrice, 'gwei');
-        console.log('gasPrice:', gasPrice,'(', gasPriceInGwei,'Gwei)');
-
-
-        const txParams = {
-            gas: await web3.utils.numberToHex(gasLimit),
-            gasPrice: await web3.utils.numberToHex(gasPrice),
-            to: receiver,
-            data: payload
-        };
-
-        const signedTx = await web3.eth.accounts.signTransaction(txParams, signer.privateKey);
-
-        return(signedTx);
-
-    } catch(error){
-        console.error('Error:', error);
-    }
-}
 
 
 // Gachaコントラクトに対して必要量のMNBCトークン使用許可を与える
 async function approveGacha(amount){
     try{
         const weiAmount = await web3.utils.toWei(amount.toString(),"ether");
-        const payload = await Coin.methods.approve(GACHA_CA, weiAmount).encodeABI();
-        //const signedTx = await createSignedTx(COIN_CA,payload,owner,150000);
+        const tx = await Coin.methods.approve(GACHA_CA, weiAmount);
+        const receipt = await sendTx(COIN_CA,tx,owner,150000);
 
-
-
-
-        //const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-
-        //console.log('tx result:',receipt);
-        console.log((`approve ${amount} MNBC tokens to GACHA_CA: ${GACHA_CA}`));
+        console.log((`approve ${amount} MNBC to GACHA_CA: ${GACHA_CA}`));
 
     } catch(error){
         console.error('Error:', error);
@@ -105,33 +133,26 @@ async function approveGacha(amount){
 }
 
 
-async function transferToken(from_account, to_address, amount){
+async function transferMNBC(to_address, amount){
     try{
         const weiAmount = web3.utils.toWei(amount.toString(),"ether");
-        //console.log(`weiAmount = ${weiAmount}`);
-        //await Coin.methods.transfer(to, weiAmount).send({from: signer.address});
-        payload = Coin.methods.transfer(to_address, weiAmount).encodeABI();
-        var signedTx = createSignedTx(COIN_CA,payload,from_account,150000);
-        const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+        const tx = Coin.methods.transfer(to_address, weiAmount);
+        const receipt = await sendTx(COIN_CA,tx,owner,150000);
 
-
-        console.log((`transferred ${amount} tokens to ${to}`));
+        console.log((`transferred ${amount} MNBC to ${to_address}`));
 
     } catch(error){
         console.error('Error:', error);
     }
 }
-
-
-
 
 async function sendManabit(to, amount, comment){
     try{
         const weiAmount = web3.utils.toWei(amount.toString(),"ether");
-        console.log(`weiAmount = ${weiAmount}`);
+        const tx = Gacha.methods.sendManabitCoin(comment, to, weiAmount);
+        const receipt = await sendTx(GACHA_CA,tx,owner,300000);
 
-        await Gacha.methods.sendManabitCoin(comment, to, weiAmount).send({from: signer.address});
-        console.log((`send ManabitCoin ${amount} tokens to ${to} with comment "${comment}"`));
+        console.log((`send Manabit ${amount} MNBC to ${to} with comment "${comment}"`));
 
     } catch(error){
         console.error("Error sending Manabit:", error);
@@ -139,67 +160,13 @@ async function sendManabit(to, amount, comment){
 }
 
 
-
-
-
-
-async function main() {
-    try {
-
-        console.log("---step1--------getOwnerBalance---------------");
-        await getOwnerBalance();
-
-        console.log("---step1b-------getAccountBalance-------------");
-        await getAccountBalance(process.env.COMMON_ADDRESS);
-
-        console.log("---step1c-------getAllowance(GACHA_CA)--------");
-        await getAllowance(GACHA_CA);
-
-
-
-
-        console.log("---step2--------approveGacha(1000)------------");
-        await approveGacha(1000);
-
-        console.log("---step2b-------getAllowance(GACHA_CA)--------");
-        await getAllowance(GACHA_CA);
-
-
-
-
-        console.log("---step3--------getOwnerBalance--------------------------");
-        await getOwnerBalance();
-
-        console.log("---step3--------getAccountBalance(MyWallet)--------------");
-        await getAccountBalance(process.env.COMMON_ADDRESS);
-
-        console.log("---step3b--------transfer 1 MNBC from OWNER to MyWallet--");
-        await transferToken(owner,process.env.MY_TEST_ADDRESS, 1);
-
-        console.log("---step3c--------getOwnerBalance-------------------------");
-        await getOwnerBalance();
-
-        console.log("---step3c--------getAccountBalance(MyWallet)-------------");
-        await getAccountBalance(process.env.COMMON_ADDRESS);
-
-
-
-
-
-        console.log("---step4-------------------------");
-        //await sendManabit("0x70997970C51812dc3A010C7d01b50e0d17dc79C8",15,"");
-
-        console.log("---step5-------------------------");
-        //await getEvent();
-        //await getAllowance(GACHA_CA);
-
-
-
-    } catch (error) {
-        console.error('Error:', error);
-    }
+module.exports = {
+    getOwnerBalance,
+    getAccountBalance,
+    getAllowance,
+    approveGacha,
+    transferMNBC,
+    sendManabit
 }
 
-// run script
-main();
 
---
+++ operation/testScript_aws.js
@@ -0,0 +1,65 @@
+require('dotenv').config({ path: '.env'});
+require('dotenv').config({ path: 'ca.env'});
+const COIN_CA = process.env.MNBC_COIN_CA;
+const GACHA_CA = process.env.MNBC_GACHA_CA;
+
+const manabitCC = require('./aws-web3-manabit') //CC=Contract Call
+
+async function main() {
+    try {
+
+        console.log("---step1--------getOwnerBalance---------------");
+        await manabitCC.getOwnerBalance();
+
+        console.log("---step1b-------getAccountBalance-------------");
+        await manabitCC.getAccountBalance(process.env.COMMON_ADDRESS);
+
+        console.log("---step1c-------getAllowance(GACHA_CA)--------");
+        await manabitCC.getAllowance(GACHA_CA);
+
+
+
+
+        console.log("---step2--------approveGacha(1000)------------");
+        //await manabitCC.approveGacha(500);
+
+        console.log("---step2b-------getAllowance(GACHA_CA)--------");
+        await manabitCC.getAllowance(GACHA_CA);
+
+        //return;
+
+        console.log("---step3--------getOwnerBalance--------------------------");
+        await manabitCC.getOwnerBalance();
+
+        console.log("---step3--------getAccountBalance(TEST Wallet)--------------");
+        await manabitCC.getAccountBalance(process.env.MY_TEST_ADDRESS);
+
+        console.log("---step3b--------transfer 1 MNBC from OWNER to TEST_Wallet--");
+        //await manabitCC.transferMNBC(process.env.MY_TEST_ADDRESS, 1);
+
+        console.log("---step3c--------getOwnerBalance-------------------------");
+        await manabitCC.getOwnerBalance();
+
+        console.log("---step3c--------getAccountBalance(TEST Wallet)-------------");
+        await manabitCC.getAccountBalance(process.env.MY_TEST_ADDRESS);
+
+        //return;
+
+
+        console.log("---step4----------sendManabit (to TEST Wallet)---------------");
+        await manabitCC.sendManabit(process.env.MY_TEST_ADDRESS,15,"2023/06/01_17:00|15 Star|mats");
+
+        console.log("---step4b--------getOwnerBalance-------------------------");
+        await manabitCC.getOwnerBalance();
+
+        console.log("---step4c--------getAccountBalance(TEST Wallet)-------------");
+        await manabitCC.getAccountBalance(process.env.MY_TEST_ADDRESS);
+
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+// run script
+main();
